700e0fad21d3142f25cbe6b4cf9dc1e13682e575
Shane Jeffery <shane.jeffery@compli.com>
2018-01-08 08:09:34 -0800

    Added shell file for generating stubs for grpc.

---
+++ bin/local/generate-stubs.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Use this script to use the proper version of PHP in Docker
+
+# docker run -it --rm -v $(pwd):/code:rw --user $(id -u) --workdir /code --entrypoint protoc snarlysodboxer/php-protoc:5.6.32 \
+docker run -it --rm -v $(pwd):/code:rw --user $(id -u) --workdir /code --entrypoint protoc snarlysodboxer/php-protoc:7.2.0 \
+    --proto_path=protos \
+    --php_out=generated \
+    --grpc_out=generated \
+    --plugin=protoc-gen-grpc=/opt/grpc_php_plugin \
+    ./protos/service-auth.proto
