0ac6caa16fe9f68ccdf56a5f3514e93e29ae49da
Shane Jeffery <shane.jeffery@compli.com>
2018-01-07 22:53:20 -0800

    Implementing user example.

--- php_client_example/client.js
+++ php_client_example/client.js
@@ -2,10 +2,10 @@
 
 const grpc = require('grpc');
 
-const test = grpc.load('./test.proto').test;
-const client = new test.greeter.Greeter('localhost:50051', grpc.credentials.createInsecure());
+const user = grpc.load('./user.proto').user;
+const client = new user.userService('localhost:50051', grpc.credentials.createInsecure());
 
-client.sayHello({name: 'Hello world', gender: 'FEMALE'}, function(error, response) {
+client.createUser({name: 'Hello world', gender: 'FEMALE'}, function(error, response) {
     if (error) {
         console.log(error.metadata.get('error'));
         console.log(error.metadata.get('code'));
