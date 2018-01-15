eec92ef54aad565e916baa7fd3668457339af58d
david amick <github@davidamick.com>
2018-01-15 19:59:01 +0000

    Update package.json

--- bin/local/npm.sh
+++ bin/local/npm.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-echo "Running the command 'docker run -it -v $(pwd):/code:rw --workdir /code --entrypoint npm ${NODE_IMAGE} $@'"
-echo
-
-docker run -i -v $(pwd):/code:rw --workdir /code --entrypoint npm node:latest $@
+docker run -i -v $(pwd):/code:rw --workdir /code --entrypoint npm node:9.2.0 $@
 
